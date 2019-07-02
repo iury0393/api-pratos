9ec24322659fa652ae2f0684ebef2b1bcbbe2845
Guest User <Guest@Andersons-MBP.mshome.net>
2019-07-02 12:53:50 -0300

    Commit inicial

---
+++ pratos-calorias/pratos-calorias/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pratos-calorias
+//
+//  Created by Guest User on 02/07/19.
+//  Copyright © 2019 Iury Vasconcelos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
