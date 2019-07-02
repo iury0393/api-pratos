bd8bea1eb561583ecf279b984715c0f2b47681ce
Guest User <Guest@Andersons-MBP.mshome.net>
2019-07-02 13:39:16 -0300

    Atualizando o storyboard e as ligações das funções

--- prato-caloria/ViewController.swift
+++ prato-caloria/ViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet var nameField : UITextField!
+    @IBOutlet var happinessField : UITextField!
+    
+    @IBAction func add() {
+        let name = nameField.text
+        let happiness = happinessField.text
+        print("eaten \(name) with \(happiness)!")
+        
     }
-
-
 }
 
