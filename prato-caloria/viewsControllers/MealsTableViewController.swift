3820df5f6a191fe7d95b34463bfda26c8b601c87
Guest User <Guest@Andersons-MBP.mshome.net>
2019-07-03 13:45:19 -0300

    Adicionando a TableView e o TableViewController

---
+++ prato-caloria/viewsControllers/MealsTableViewController.swift
@@ -0,0 +1,28 @@
+//
+//  MealsTableViewController.swift
+//  prato-caloria
+//
+//  Created by Guest Iury Vasconcelos on 03/07/19.
+//  Copyright © 2019 Iury Vasconcelos. All rights reserved.
+//
+
+import UIKit
+
+class MealsTableViewController : UITableViewController {
+    var meals = [Meal(name: "Eggplant Brownie", happiness: 5),
+                 Meal(name: "Zucchini Muffin", happiness: 3)]
+    
+    override func tableView(_ tableView: UITableView,
+                            numberOfRowsInSection section: Int) -> Int {
+        return meals.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let row = indexPath.row
+        let meal = meals[row]
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default,
+                                   reuseIdentifier: nil)
+        cell.textLabel!.text = meal.name //Adicionamos o '.name' para exibir apenas o nome da refeição na tabela
+        return cell
+    }
+}
