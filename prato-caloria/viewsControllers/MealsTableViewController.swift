cdb0afb557be3ac8e62d49555936fe94c5ee9bd4
Iury Vasconcelos <iury.vasconcelos@geq.com.br>
2019-07-09 11:40:57 -0300

    Adicionando a ação de longpress

--- prato-caloria/viewsControllers/MealsTableViewController.swift
+++ prato-caloria/viewsControllers/MealsTableViewController.swift
@@ -15,26 +15,40 @@ class MealsTableViewController : UITableViewController, AddAMealDelegate {
     func add(_ meal:Meal) {
         meals.append(meal)
         tableView.reloadData()
-    }// Adiciona a refeição no array e atualiza a tabela
+    }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if(segue.identifier == "addMeal") {
             let view = segue.destination as! ViewController
             view.delegate = self
         }
-    }// Cria um segue que é ativado assim que o botão para mudar de tela é clicado, antes de mudar de tela, no caso para a tela de adicionar
+    }
     
     override func tableView(_ tableView: UITableView,
                             numberOfRowsInSection section: Int) -> Int {
         return meals.count
-    } //Retorna o número de linhas preenchido na tabela
+    }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let row = indexPath.row //Pega a posição da linha
+        let row = indexPath.row
         let meal = meals[row]
-        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default,
-                                   reuseIdentifier: nil)
-        cell.textLabel!.text = meal.name //Adicionamos o '.name' para exibir apenas o nome da refeição na tabela
+        let cell = UITableViewCell(style:UITableViewCell.CellStyle.default, reuseIdentifier: nil)
+        cell.textLabel!.text = meal.name
+        
+        let recognizer = UILongPressGestureRecognizer(target: self, action: #selector(showDetails))
+        cell.addGestureRecognizer(recognizer)
+        
         return cell
-    }// Popula a nova refeição na tabela
+    }
+    
+    @objc func showDetails(recognizer: UILongPressGestureRecognizer) {
+        if(recognizer.state == UIGestureRecognizer.State.began) {
+            let cell = recognizer.view as! UITableViewCell
+            if let indexPath = tableView.indexPath(for: cell) {
+                let row = indexPath.row
+                let meal = meals[row]
+                print("Long press: \(meal.name))")
+            }
+        }
+    }
 }
